d66ca2db95cb59d343065a8b449d8de3595179e7
AmodKumarJ <amodkumarjg@gmail.com>
2025-09-25 15:47:47 +0530

    changes-25-09-2025

--- portfolio-backend/src/controllers/portfolio.controller.ts
+++ portfolio-backend/src/controllers/portfolio.controller.ts
@@ -8,6 +8,8 @@ import { Stock, SectorSummary } from "../models/stock.model";
 const FILE_PATH = path.join(__dirname, "../../portfolio.xlsx");
 
 
+// MAPPING FOR STOCK SYMBOLS
+
 const STOCK_SYMBOL_MAPPING: { [key: string]: string } = {
   // Financial Sector changes added here
   "HDFC Bank": "HDFCBANK",
